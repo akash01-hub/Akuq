d168716172b45760529dbedbe2340089d86626e9
akash01-hub <mauryaakashkumar458@gmail.com>
2025-04-10 11:42:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+questions = []  # Simple in-memory storage
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        question = request.form['question']
+        if question:
+            questions.append(question)
+        return redirect('/')
+    return render_template('index.html', questions=questions)
+
+if __name__ == '__main__':
+    app.run(debug=True)
